ece013c96b516d0ee69de3b45d8eb1902444f54d
MiKoKappa <mikopar@gmail.com>
2020-07-22 10:54:21 +0200

    Updates

---
+++ precache-manifest.f957ebf8da21489975ee564f8fc13990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892e396c0bb7e6239593622a269d2695",
+    "url": "/movit/index.html"
+  },
+  {
+    "revision": "e0294e5f3685afc80bc9",
+    "url": "/movit/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbd0f71a3143f8a1664c",
+    "url": "/movit/static/js/2.b4de12f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movit/static/js/2.b4de12f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0294e5f3685afc80bc9",
+    "url": "/movit/static/js/main.de9cd2cc.chunk.js"
+  },
+  {
+    "revision": "6536b2b240b8b0d1683a",
+    "url": "/movit/static/js/runtime-main.54cb43a9.js"
+  },
+  {
+    "revision": "6f66a8e3df9eca233e2fed1c0d401ee7",
+    "url": "/movit/static/media/movit_logo.6f66a8e3.svg"
+  }
+]);

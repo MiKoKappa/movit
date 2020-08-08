1eb6073a38cba7066a64c3799cc1604f0b3cacec
MiKoKappa <mikopar@gmail.com>
2020-08-08 19:17:30 +0200

    add loader

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState, useEffect } from "react";
 import MovieDetails from "./MovieDetails";
 import useStyles from "../styles/MoviePageStyles";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function MoviePage(props) {
   const classes = useStyles();
@@ -25,43 +27,50 @@ export default function MoviePage(props) {
   } else {
     document.title = "MOVit";
   }
-  return (
-    <div className={classes.moviewindowwrapper}>
-      {movie.title && window.innerWidth > 528 ? (
-        <div
-          style={{
-            width: "100vw",
-            height: "100vh",
-            position: "absolute",
-            zIndex: "0",
-            background: `radial-gradient(at 15% 50%, rgba(0, 0, 0, 0.2) 0%, rgb(22, 22, 22) 60%), url(${
-              movie.backdrop_path
-                ? "https://image.tmdb.org/t/p/original" + movie.backdrop_path
-                : ""
-            }) no-repeat left`,
-            backgroundSize: "contain",
-          }}
-        ></div>
-      ) : (
-        ""
-      )}
-      {movie.title ? <MovieDetails props={movie} /> : ""}
-      <div className={classes.signatures}>
-        <h3>
-          {localStorage.getItem("lang") === "en" ? "Built by" : "Wykonał"}{" "}
-          <a href="https://github.com/MiKoKappa">
-            Mikołaj Tkaczyk <i class="fab fa-github"></i>
-          </a>
-        </h3>
-        <h3>
-          {localStorage.getItem("lang") === "en"
-            ? "Data provided by"
-            : "Informacje z"}{" "}
-          <a href="https://www.themoviedb.org">
-            TMDB <i class="fas fa-info"></i>
-          </a>
-        </h3>
+  if (movie.title) {
+    return (
+      <div className={classes.moviewindowwrapper}>
+        {movie.title && window.innerWidth > 528 ? (
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              position: "absolute",
+              zIndex: "0",
+              background: `radial-gradient(at 5% 50%, rgba(0, 0, 0, 0.2) 0%, rgb(22, 22, 22) 60%), url(${
+                movie.backdrop_path
+                  ? "https://image.tmdb.org/t/p/original" + movie.backdrop_path
+                  : ""
+              }) no-repeat left`,
+              backgroundSize: "contain",
+            }}
+          ></div>
+        ) : (
+          ""
+        )}
+        {movie.title ? <MovieDetails props={movie} /> : ""}
+        <div className={classes.signatures}>
+          <h3>
+            {localStorage.getItem("lang") === "en" ? "Built by" : "Wykonał"}{" "}
+            <a href="https://github.com/MiKoKappa">
+              Mikołaj Tkaczyk <i class="fab fa-github"></i>
+            </a>
+          </h3>
+          <h3>
+            {localStorage.getItem("lang") === "en"
+              ? "Data provided by"
+              : "Informacje z"}{" "}
+            <a href="https://www.themoviedb.org">
+              TMDB <i class="fas fa-info"></i>
+            </a>
+          </h3>
+        </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    <Backdrop open={true}>
+      <CircularProgress color="#FFF" />
+    </Backdrop>
   );
 }
